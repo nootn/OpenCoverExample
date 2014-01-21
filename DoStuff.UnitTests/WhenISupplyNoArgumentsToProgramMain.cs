6e628f23a41316be756a6b41f717029b1f732677
Andrew Newton <me@nootn.com.au>
2014-01-21 11:27:00 +1000

    Adds unit tests project with two passing tests

---
+++ DoStuff.UnitTests/WhenISupplyNoArgumentsToProgramMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoStuff.UnitTests
+{
+    [TestFixture]
+    public class WhenISupplyNoArgumentsToProgramMain
+    {
+
+        private StringBuilder _output = new StringBuilder();
+
+        [SetUp]
+        public void Setup()
+        {
+            System.Console.SetOut(new StringWriter(_output));
+            DoStuff.Console.Program.Main(null);
+        }
+
+        [Test]
+        public void ThenNothingIsWrittenToConsole()
+        {
+            Assert.IsEmpty(_output.ToString());
+        }
+
+    }
+}
---
+++ DoStuff.UnitTests/WhenISupplyOneValidArgumentsToProgramMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoStuff.UnitTests
+{
+    [TestFixture]
+    public class WhenISupplyOneValidArgumentsToProgramMain
+    {
+
+        private StringBuilder _output = new StringBuilder();
+
+        [SetUp]
+        public void Setup()
+        {
+            System.Console.SetOut(new StringWriter(_output));
+            DoStuff.Console.Program.Main(new []{"test"});
+        }
+
+        [Test]
+        public void ThenSomethingIsWrittenToConsole()
+        {
+            Assert.IsNotEmpty(_output.ToString());
+        }
+
+    }
+}

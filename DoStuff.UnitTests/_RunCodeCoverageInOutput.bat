0bb136afb52d625255f6232e03538479db1b9d7a
Andrew Newton <me@nootn.com.au>
2014-01-21 11:45:27 +1000

    Adds code coverage generator batch file, and commits an example output

---
+++ DoStuff.UnitTests/_RunCodeCoverageInOutput.bat
@@ -0,0 +1,2 @@
+"..\..\..\packages\OpenCover.4.5.2316\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" -targetargs:"/nologo DoStuff.UnitTests.dll /noshadow" -filter:"+[DoStuff.Console]DoStuff.Console*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult.xml"
+"..\..\..\packages\ReportGenerator.1.9.1.0\ReportGenerator.exe" "-reports:_CodeCoverageResult.xml" "-targetdir:_CodeCoverageReport"

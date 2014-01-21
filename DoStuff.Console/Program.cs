33492e19d8ef8a9921077d6a4863c0a8a0b19a12
Andrew Newton <me@nootn.com.au>
2014-01-21 11:22:54 +1000

    Adds basic console app

---
+++ DoStuff.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoStuff.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stuffDoer = new StuffDoer();
+            if (args != null && args.Length > 0)
+            {
+                stuffDoer.DoTheStuff();
+            }
+        }
+    }
+}
---
+++ DoStuff.Console/StuffDoer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoStuff.Console
+{
+    class StuffDoer
+    {
+        public void DoTheStuff()
+        {
+            System.Console.WriteLine("I am doing stuff");
+        }
+    }
+}
